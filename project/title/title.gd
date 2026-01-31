3e400b257fcb84e7930e827861fba17d68f5041f
Paul Gestwicki <pvgestwicki@bsu.edu>
2026-01-31 14:42:41 -0500

    Make garish title animation

--- project/title/title.gd
+++ project/title/title.gd
@@ -1,7 +1,25 @@
 extends Control
 
+var COLORS : Array[Color] = [
+	# Santo blue
+	Color.html("8ed2e5"), 
+	# Bright red
+	Color.html("dc263b"),
+	# Bright green
+	Color.html("009f59"),
+	# Bright yellow
+	Color.html("cdca55"),
+]
+
 @export var main_game_scene : PackedScene
+
+var _color_index := 0
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed(&"start"):
 		get_tree().change_scene_to_packed(main_game_scene)
+
+
+func _on_color_flash_timer_timeout() -> void:
+	_color_index = (_color_index + 1) % COLORS.size()
+	%TitleLabel.label_settings.font_color = COLORS[_color_index]
