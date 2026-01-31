9ae24f121b131a88c821b3a4ce3439cefb685ed4
Paul Gestwicki <pvgestwicki@bsu.edu>
2026-01-31 07:02:16 -0500

    Add clarifying comment

--- project/guy/guy.gd
+++ project/guy/guy.gd
@@ -19,6 +19,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	# If not being pushed, the player has control of the character.
 	if push_vector==Vector2.ZERO:
 		# Handle jump.
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
